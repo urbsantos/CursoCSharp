73a1c1f76a6b0ad1c33caac51558c699cae7aeb6
urbsantos <urbanolopes@gmail.com>
2020-05-25 09:35:19 -0300

    Adding more exercises
    Adding more exercises

---
+++ EstruturaDeControle/UsandoContinue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturaDeControle {
+    class UsandoContinue {
+        public static void Executar() {
+            int internalo = 50;
+            Console.WriteLine("Números pares entre 1 e {0}", internalo);
+
+            for (int i = 1; i <= internalo; i++) {
+                if (i % 2 == 1) {
+                    continue;
+                }
+
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -31,6 +31,11 @@ namespace CursoCSharp {
                 {"Estrutura If/Else/If - Estruturas de Controle", EstruturaIfElseIf.Executar},
                 {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
                 {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
+                {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
+                {"Estrutura Foreach - Estruturas de Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
             });
 
             central.SelecionarEExecutar();
