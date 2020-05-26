d31801547452232eba73e813a2437e017777d66e
urbsantos <urbanolopes@gmail.com>
2020-05-25 21:19:40 -0300

    Object oriented programming
    Object oriented programming

---
+++ ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class Pessoa {
+
+        public string Nome;
+        public int Idade;
+
+        public string Apresentar() {
+            
+            if (Nome != "" || Idade != 0) {
+                return string.Format(
+                $"Olá! Me chamo {Nome} e tenho {Idade} anos.");
+            } else {
+                return string.Format("Dados inválidos");
+                
+            }
+        }
+
+        public void ApresentarNoConsole() {
+            Console.WriteLine(Apresentar());
+        }
+
+        public void Zerar() {
+            Nome = "";
+            Idade = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
 using CursoCSharp.EstruturaDeControle;
+using CursoCSharp.ClassesEMetodos;
 
 namespace CursoCSharp {
     class Program {
@@ -36,6 +37,10 @@ namespace CursoCSharp {
                 {"Estrutura Foreach - Estruturas de Controle", EstruturaForEach.Executar},
                 {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
                 {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
+                
+                //Classes e Métodos
+                {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
             });
 
             central.SelecionarEExecutar();
